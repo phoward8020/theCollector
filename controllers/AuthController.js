a2a8583ce9e8da82f0e55087037339009675f25e
Paul Howard <paul@paul-howard.net>
2014-12-15 14:16:17 -0800

    Add logic to auth/post_signup

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,12 +1,41 @@
-var debug   = require('debug');
+var debug   = require('debug')
+  , db      = require("../models/index.js");
 
 module.exports = {
-    get_signup : function(req, res) {
-        res.send('Got to auth/signup GET');
-    },
 
     post_signup : function(req, res) {
         debug('>> Routing to /auth/signup (POST)');
-        res.send('Got to auth/signup POST.');
+        // res.send('req.body.email: ', req.body.email);
+
+        db.user.findOrCreate(
+        {
+            where: {
+                email:req.body.email
+            },
+            defaults: {
+                email:req.body.email,
+                password:req.body.password
+            }
+        }).spread(function(user, created) {
+            if (!created) {
+                req.flash('danger', 'Email already exists in database!');
+                res.redirect('/');
+            } else {
+                req.flash('info', 'User created successfully.');
+                res.redirect('/');
+            }
+        }).catch(function(error) {
+            if (error && Array.isArray(error.errors)) {
+                error.errors.forEach(function(errorItem) {
+                    req.flash('danger', errorItem.message);
+                    debug('>>>>> ERROR: ', errorItem.message);
+                })
+            } else {
+                req.flash('danger', 'Unknown error')
+            };
+            res.redirect('/');
+        });
+
     }
+
 }
