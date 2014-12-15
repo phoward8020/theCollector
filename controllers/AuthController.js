e3597cedbc1a3246839889b9b7af0b2e8fcd2d88
Paul Howard <paul@paul-howard.net>
2014-12-15 15:58:54 -0800

    Complete minimal login process

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 var debug   = require('debug')
+  , bcrypt  = require('bcrypt')
   , db      = require("../models/index.js");
 
 module.exports = {
@@ -36,7 +37,37 @@ module.exports = {
     },  // << End 'post_signup'
 
     post_login : function(req, res) {
-        res.send('Got to post_login route.');
+        // res.send('Got to post_login route.');
+        //do login here (check password and set session value)
+        db.user.find({where:{email:req.body.email}}).then(function(userObj) {
+            if (userObj) {
+                // check password now
+                // res.send("we will test password now.")
+                bcrypt.compare(req.body.password, userObj.password, function(err, match) {
+                    if (match) {
+                        // store userObj in session here.
+                        // res.send("password is correct!");
+                        req.session.user = {
+                            id: userObj.id,
+                            email: userObj.email,
+                            name: userObj.name
+                        };
+                        res.redirect("/");
+                    } else {
+                        req.flash('danger', 'Invalid Password!');
+                        res.redirect("/");
+                        // res.send("invalid password!");
+                    }
+                })
+            } else {
+                // error - user not found.
+                req.flash('danger', 'Unknown User!');
+                res.redirect("/");
+                // res.send("Unknown user.");
+            }
+        });
+        //user is logged in forward them to the home page
+        // res.redirect('/');
     }   // << End 'post_login'
 
 }
--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -3,8 +3,9 @@ var debug   = require('debug');
 module.exports = {
     get_index : function(req, res) {
         debug('>> Routing to /');
+        var user = req.getUser();
         var key = process.env.comic_vine_api_key;
         var searchURL = "http://www.comicvine.com/api/issues/?format=json&sort=id&api_key=" + key;
-        res.render('index', {'searchURL': searchURL});
+        res.render('index', {'searchURL': searchURL, 'user':user});
     }
 }
